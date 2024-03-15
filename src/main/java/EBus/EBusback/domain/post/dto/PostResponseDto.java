1891db4e4c67c43a7ecbe704d6b6dcb9601608b3
yeji <yeji.lee.kim@gmail.com>
2024-03-16 00:52:12 +0900

    feat: 건의/감사 글 response dto 생성

---
+++ src/main/java/EBus/EBusback/domain/post/dto/PostResponseDto.java
@@ -0,0 +1,17 @@
+package EBus.EBusback.domain.post.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostResponseDto {
+
+	private Long postId;
+	private String title;
+	private String content;
+	private Boolean isSuggestion;
+	private LocalDateTime createdDate;
+}
