1f12016c7d8fc455e973b1f53209eaef0925e20c
yeji <yeji.lee.kim@gmail.com>
2024-03-15 16:40:31 +0900

    feat: project setting

---
+++ src/main/java/EBus/EBusback/EBusBackApplication.java
@@ -0,0 +1,13 @@
+package EBus.EBusback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBusBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EBus/EBusback/EBusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package EBus.EBusback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
