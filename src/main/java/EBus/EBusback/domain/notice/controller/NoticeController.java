87d3406af0c726e421a19a0535924ee0f36bbcf1
yeji <yeji.lee.kim@gmail.com>
2024-03-16 05:25:18 +0900

    feat: 공지사항 삭제를 위한 컨트롤러 메서드 추가

--- src/main/java/EBus/EBusback/domain/notice/controller/NoticeController.java
+++ src/main/java/EBus/EBusback/domain/notice/controller/NoticeController.java
@@ -3,6 +3,7 @@ package EBus.EBusback.domain.notice.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,12 @@ public class NoticeController {
 	@ResponseStatus(HttpStatus.OK)
 	public List<NoticeResponseDto> findNoticeList() {
 		return noticeService.findNoticeList();
+	}
+
+	@DeleteMapping("/{noticeId}")
+	@ResponseStatus(HttpStatus.OK)
+	public String removeNotice(@PathVariable Long noticeId) {
+		noticeService.removeNotice(noticeId);
+		return "공지사항이 삭제되었습니다.";
 	}
 }
