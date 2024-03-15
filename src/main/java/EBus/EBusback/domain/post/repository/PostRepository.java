c1d4535cf91a1c7905164db6f88014591bdb4cb2
yeji <yeji.lee.kim@gmail.com>
2024-03-16 01:01:21 +0900

    feat: PostRepository 생성

---
+++ src/main/java/EBus/EBusback/domain/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package EBus.EBusback.domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import EBus.EBusback.domain.post.entity.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
