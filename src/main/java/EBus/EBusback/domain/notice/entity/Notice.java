95086ffbe1cea850e2a5306d76614383607319a5
yeji <yeji.lee.kim@gmail.com>
2024-03-15 17:12:15 +0900

    feat: Notice 엔티티 생성

---
+++ src/main/java/EBus/EBusback/domain/notice/entity/Notice.java
@@ -0,0 +1,33 @@
+package EBus.EBusback.domain.notice.entity;
+
+import EBus.EBusback.domain.member.entity.Member;
+import EBus.EBusback.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Notice extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long noticeId;
+
+	@Column(nullable = false, length = 30)
+	private String title;
+
+	@Column(nullable = false, length = 100)
+	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+}
