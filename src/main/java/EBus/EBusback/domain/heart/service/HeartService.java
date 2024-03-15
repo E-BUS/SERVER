63c8a83db776d56bdf5d024ca85d83f1ca5cd92d
yeji <yeji.lee.kim@gmail.com>
2024-03-16 02:32:46 +0900

    feat: 좋아요 등록/취소 서비스 메서드 추가

---
+++ src/main/java/EBus/EBusback/domain/heart/service/HeartService.java
@@ -0,0 +1,40 @@
+package EBus.EBusback.domain.heart.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import EBus.EBusback.domain.heart.entity.Heart;
+import EBus.EBusback.domain.heart.repository.HeartRepository;
+import EBus.EBusback.domain.member.entity.Member;
+import EBus.EBusback.domain.post.entity.Post;
+import EBus.EBusback.domain.post.repository.PostRepository;
+import EBus.EBusback.global.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class HeartService {
+
+	private final HeartRepository heartRepository;
+	private final PostRepository postRepository;
+
+	public String createOrRemoveHeart(Long postId) {
+		Member member = SecurityUtil.getCurrentUser();
+		if (member == null)
+			throw new RuntimeException("사용자를 찾을 수 없습니다.");
+
+		Post post = postRepository.findById(postId)
+			.orElseThrow(() -> new IllegalArgumentException("글을 찾을 수 없습니다."));
+
+		Heart heart = heartRepository.findByMemberAndPost(member, post)
+			.orElseGet(() -> heartRepository.save(Heart.builder().member(member).post(post).build()));
+
+		heart.updateHeart();
+
+		if (heart.getIsValid())
+			return "좋아요가 등록되었습니다.";
+		else
+			return "좋아요가 취소되었습니다.";
+	}
+}
