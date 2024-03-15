55920983731b6a045d9de7fbe8a30aabc310d264
yeji <yeji.lee.kim@gmail.com>
2024-03-16 02:02:29 +0900

    feat: 건의/감사 글에서 사용자와 연관된 정보를 담는 Dto 생성

---
+++ src/main/java/EBus/EBusback/domain/post/dto/PostMemberDto.java
@@ -0,0 +1,12 @@
+package EBus.EBusback.domain.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostMemberDto {
+
+	private Boolean hasHeart;
+	private Boolean isWriter;
+}
