f06218cffa7ff9974ceb31fb85f2dc7e33eeb3d8
yeji <yeji.lee.kim@gmail.com>
2024-03-16 19:53:58 +0900

    feat: posts, hearts, notices api 스웨거 설명 추가

--- src/main/java/EBus/EBusback/domain/heart/controller/HeartController.java
+++ src/main/java/EBus/EBusback/domain/heart/controller/HeartController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import EBus.EBusback.domain.heart.dto.HeartRequestDto;
 import EBus.EBusback.domain.heart.service.HeartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "건의해요/고마워요", description = "건의해요/고마워요 글 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/hearts")
@@ -17,6 +22,13 @@ public class HeartController {
 
 	private final HeartService heartService;
 
+	@Operation(summary = "좋아요 등록/취소")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "201", description = "좋아요 등록"),
+		@ApiResponse(responseCode = "201", description = "좋아요 취소"),
+		@ApiResponse(responseCode = "401", description = "로그인 필요"),
+		@ApiResponse(responseCode = "404", description = "해당 id를 갖는 글 없음")
+	})
 	@PostMapping
 	public String clickHeart(@RequestBody @Valid HeartRequestDto requestDto) {
 		return heartService.createOrRemoveHeart(requestDto.getPostId());
--- src/main/java/EBus/EBusback/domain/post/controller/PostController.java
+++ src/main/java/EBus/EBusback/domain/post/controller/PostController.java
@@ -17,9 +17,14 @@ import EBus.EBusback.domain.post.dto.PostDetailResponseDto;
 import EBus.EBusback.domain.post.dto.PostOutlineResponseDto;
 import EBus.EBusback.domain.post.dto.PostRequestDto;
 import EBus.EBusback.domain.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "건의해요/고마워요", description = "건의해요/고마워요 글 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
@@ -27,36 +32,62 @@ public class PostController {
 
 	private final PostService postService;
 
+	@Operation(summary = "건의해요 글 등록")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "201", description = "등록 성공"),
+		@ApiResponse(responseCode = "401", description = "로그인 필요")
+	})
 	@PostMapping("/suggestion")
 	@ResponseStatus(HttpStatus.CREATED)
 	public PostCreateResponseDto createSuggestion(@RequestBody @Valid PostRequestDto requestDto) {
 		return postService.createPost(requestDto, true);
 	}
 
+	@Operation(summary = "고마워요 글 등록")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "201", description = "등록 성공"),
+		@ApiResponse(responseCode = "401", description = "로그인 필요")
+	})
 	@PostMapping("/appreciation")
 	@ResponseStatus(HttpStatus.CREATED)
 	public PostCreateResponseDto createAppreciation(@RequestBody @Valid PostRequestDto requestDto) {
 		return postService.createPost(requestDto, false);
 	}
 
+	@Operation(summary = "건의해요/고마워요 글 상세 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "조회 성공"),
+		@ApiResponse(responseCode = "404", description = "해당 id를 갖는 글 없음")
+	})
 	@GetMapping("/{postId}")
 	@ResponseStatus(HttpStatus.OK)
 	public PostDetailResponseDto findPost(@PathVariable Long postId) {
 		return postService.findPost(postId);
 	}
 
+	@Operation(summary = "건의해요 글 리스트 조회")
+	@ApiResponse(responseCode = "200", description = "조회 성공")
 	@GetMapping("/suggestion")
 	@ResponseStatus(HttpStatus.OK)
 	public List<PostOutlineResponseDto> findSuggestionList() {
 		return postService.findPostList(true);
 	}
 
+	@Operation(summary = "고마워요 글 리스트 조회")
+	@ApiResponse(responseCode = "200", description = "조회 성공")
 	@GetMapping("/appreciation")
 	@ResponseStatus(HttpStatus.OK)
 	public List<PostOutlineResponseDto> findAppreciationList() {
 		return postService.findPostList(false);
 	}
 
+	@Operation(summary = "건의해요/고마워요 글 삭제")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "삭제 성공"),
+		@ApiResponse(responseCode = "401", description = "로그인 필요"),
+		@ApiResponse(responseCode = "403", description = "작성자 아님"),
+		@ApiResponse(responseCode = "404", description = "해당 id를 갖는 글 없음")
+	})
 	@DeleteMapping("/{postId}")
 	@ResponseStatus(HttpStatus.OK)
 	public String removePost(@PathVariable Long postId) {
