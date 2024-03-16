682beb1a283332d147ec57d271e1cf0943025098
yeji <yeji.lee.kim@gmail.com>
2024-03-16 21:05:33 +0900

    style: NoticeService에 주석 추가

--- src/main/java/EBus/EBusback/domain/notice/service/NoticeService.java
+++ src/main/java/EBus/EBusback/domain/notice/service/NoticeService.java
@@ -24,17 +24,19 @@ public class NoticeService {
 
 	private final NoticeRepository noticeRepository;
 
+	// 공지사항 등록
 	public NoticeResponseDto createNotice(PostRequestDto requestDto) {
 		Member member = SecurityUtil.getCurrentUser();
 		if (member == null)
 			throw new ResponseStatusException(ErrorCode.NON_LOGIN.getStatus(), ErrorCode.NON_LOGIN.getMessage());
-		if (!member.getRole().equals(Role.ADMIN))
+		if (!member.getRole().equals(Role.ADMIN))  // 사용자가 관리자인지 확인
 			throw new ResponseStatusException(ErrorCode.NO_ADMIN.getStatus(), ErrorCode.NO_ADMIN.getMessage());
 		return new NoticeResponseDto(noticeRepository.save(
 			Notice.builder().title(requestDto.getTitle()).content(requestDto.getContent()).writer(member).build()
 		));
 	}
 
+	// noticeId를 id로 가지는 공지사항 상세 조회
 	public NoticeResponseDto findNotice(Long noticeId) {
 		Notice notice = noticeRepository.findById(noticeId)
 			.orElseThrow(() -> new ResponseStatusException(
@@ -42,11 +44,13 @@ public class NoticeService {
 		return new NoticeResponseDto(notice);
 	}
 
+	// 공지사항 전체 조회
 	public List<NoticeResponseDto> findNoticeList() {
 		return noticeRepository.findAll().stream()
 			.map(NoticeResponseDto::new).collect(Collectors.toList());
 	}
 
+	// noticeId를 id로 가지는 공지사항 삭제
 	public void removeNotice(Long noticeId) {
 		Member member = SecurityUtil.getCurrentUser();
 		if (member == null)
@@ -54,7 +58,7 @@ public class NoticeService {
 		Notice notice = noticeRepository.findById(noticeId)
 			.orElseThrow(() -> new ResponseStatusException(
 				ErrorCode.NO_NOTICE_EXIST.getStatus(), ErrorCode.NO_NOTICE_EXIST.getMessage()));
-		if (!member.getRole().equals(Role.ADMIN))
+		if (!member.getRole().equals(Role.ADMIN))  // 사용자가 관리자인지 확인
 			throw new ResponseStatusException(ErrorCode.NO_ADMIN.getStatus(), ErrorCode.NO_ADMIN.getMessage());
 		noticeRepository.delete(notice);
 	}
