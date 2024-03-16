65f9964b1c9acdeb5e43562a77b26dc938bf1cc0
yeji <yeji.lee.kim@gmail.com>
2024-03-16 14:51:22 +0900

    feat: HeartRequestDto 검증

--- src/main/java/EBus/EBusback/domain/heart/dto/HeartRequestDto.java
+++ src/main/java/EBus/EBusback/domain/heart/dto/HeartRequestDto.java
@@ -1,9 +1,11 @@
 package EBus.EBusback.domain.heart.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class HeartRequestDto {
 
+	@NotNull
 	private Long postId;
 }
