804178708d856f98d93125596d55e85ab65f2d07
yeji <yeji.lee.kim@gmail.com>
2024-03-16 14:45:51 +0900

    feat: PostRequestDto 검증

--- src/main/java/EBus/EBusback/domain/post/dto/PostRequestDto.java
+++ src/main/java/EBus/EBusback/domain/post/dto/PostRequestDto.java
@@ -1,16 +1,19 @@
 package EBus.EBusback.domain.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PostRequestDto {
 
+	@NotEmpty
 	@NotBlank
 	@Size(min = 1, max = 30)
 	private String title;
 
+	@NotEmpty
 	@NotBlank
 	@Size(min = 1, max = 100)
 	private String content;
